89ea932788e743e0817b443e2dad2568b91d241d
CursedMushroom <osl2877@rit.edu>
2023-02-19 15:38:15 -0500

    got adding a retreving users with all data provided to work

--- src/server.js
+++ src/server.js
@@ -1,27 +1,75 @@
 const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
 const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-
+// head request and body demo
 const urlStruct = {
-    'GET': {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        // /default -> all other 404
-        // /getusers
-        // /notReal
-    },
-    'HEAD': {
-        // /getusers
-        // /notReal
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notFound,
+    notFound: jsonHandler.notFound, // -> all other 404
+    index: htmlHandler.getIndex,
 
-    },
-    'POST':{
-        // /addUser
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notFound,
+    notFound: jsonHandler.notFoundMeta, // -> all other 404
 
-    }
+  },
+
+};
+/// ---Post-----//
+const parseBody = (request, response, handlerFunction) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handlerFunction(request, response, bodyParams);
+  });
 };
 
-// http.createServer(onRequest).listen(port, () => {
-//     console.log(`Listening on 127.0.0.1: ${port}`);
-// });
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+// -------------------//
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    return handlePost(request, response, parsedUrl);
+  }
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD.notFound(request, response);
+  }
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  }
+  return null;
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
